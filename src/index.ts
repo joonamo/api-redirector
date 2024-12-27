297daa140e83b62c078c1b9732433075b8b66de5
joonamo <joonamo@users.noreply.github.com>
2024-12-27 17:52:52 +0200

    Implement simple HTTP redirect based on example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// Based on https://developers.cloudflare.com/workers/examples/bulk-redirects/
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request): Promise<Response> {
+    const targetApiBaseUrl = "https://xblvulfnrlbhmjenqysx.supabase.co/functions/v1/scores";
+
+    const requestURL = new URL(request.url);
+
+		const targetUrl = new URL(requestURL.pathname + requestURL.search, targetApiBaseUrl).toString()
+		console.log(`Redirected to targetUrl ${targetUrl}`)
+		
+		return Response.redirect(targetUrl, 302);
+  },
+} satisfies ExportedHandler;
